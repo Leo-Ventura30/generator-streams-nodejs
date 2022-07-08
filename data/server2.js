2ab3e03931b2888c8687168359461346a011aa51
Leo Ventura <lfventura@huia.haus>
2022-07-07 23:41:46 -0300

    feat: gerando server2

---
+++ data/server2.js
@@ -0,0 +1,18 @@
+// curl -X POST "localhost:4000/cart" --data '{"id":"123"}'
+const {createServer} = require('http')
+const PORT = 4000
+async function handler(request, response) {
+  if (
+      request.method === 'POST' &&
+      request.url.includes('cart')
+    ) {
+      for await (const data of request){
+        const item = JSON.parse(data)
+        console.log('response', item)
+        return response.end(`proccessed item ${item.id}`)
+      }
+    }
+  return response.end('hey')
+} 
+
+createServer(handler).listen(PORT, () => console.log('CART UP ON PORT',PORT))
