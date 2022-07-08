460cfdb6868923710e7a5db471d4d65ce913b680
Leo Ventura <lfventura@huia.haus>
2022-07-08 00:25:13 -0300

    feat: implement generator function

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const {createServer} = require('http'),
-      {parse} = require('url'),
-      {randomUUID} = require("crypto")
+const axios = require('axios')
 
-async function handler(request, response) {
-  if (
-      request.method === 'GET' &&
-      request.url.includes('products')
-    ) {
-      const {query:{productName}} = parse(request.url, true)
-      const result = {
-        id: randomUUID(),
-        product: productName
-      }
-      return response.end(JSON.stringify({result}))
-  } 
-  return response.end('hey')
+const myDB = async ()=> Array.from({length:1000}, (v, index)=> `${index}-cellphone`)
 
+const PRODUCTS_URL = 'http://localhost:3000/products'
+const CART_URL = 'http://localhost:4000/cart'
+
+async function processData(){
+  const products = await myDB()
+  const response = []
+  for (const product of products) {
+    const { data: productInfo } = await axios.get(`${PRODUCTS_URL}?productName=${product}`)
+    const { data: cartData } = await axios.post(`${CART_URL}`, productInfo)
+    console.log('product info', productInfo)
+    response.push(cartData)
+  }
 }
-createServer(handler).listen(3000,()=>console.log('ON'))
+// (async()=> {
+//   await processData()
+// })()
+
+async function* processDataGenerator(){
+  const products = await myDB()
+  const response = []
+  for (const product of products) {
+    const { data: productInfo } = await axios.get(`${PRODUCTS_URL}?productName=${product}`)
+    const { data: cartData } = await axios.post(`${CART_URL}`, productInfo)
+    console.log('product info', productInfo)
+    yield cartData
+  }
+}
+(async()=>{
+    for await(const data of processDataGenerator()){
+      setTimeout(()=>"hey",1000)
+      console.table(data)
+    }
+  }
+)()
